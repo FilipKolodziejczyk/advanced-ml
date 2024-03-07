593f6f6651781129d4332b7151995fe356962b2d
Filip Kołodziejczyk <filip.kolodziejczyk.00@gmail.com>
2024-03-07 18:36:31 +0100

    Added bias to logistic regression and using loss function for convergence

--- project1/optimizer.py
+++ project1/optimizer.py
@@ -5,6 +5,6 @@ class Optimizer(ABC):
     """An abstract class for ML model optimizers"""
 
     @abstractmethod
-    def update(self, X, y, weights, predictions):
-        """Update the model's weights based on the given data and model"""
+    def update(self, X, y, y_pred, params):
+        """Update the model's parameters based on the given data and model"""
         raise NotImplementedError("Subclasses must implement this method")
