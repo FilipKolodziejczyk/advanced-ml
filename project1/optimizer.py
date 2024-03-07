f43639259c20d38996a55eead66aa271756463f6
Filip Kołodziejczyk <filip.kolodziejczyk.00@gmail.com>
2024-03-07 19:22:40 +0100

    Moved shuffling data and gradient calculation to optimizer base class (code redundancy)

--- project1/optimizer.py
+++ project1/optimizer.py
@@ -1,8 +1,26 @@
 from abc import ABC, abstractmethod
+import numpy as np
 
 
 class Optimizer(ABC):
     """An abstract class for ML model optimizers"""
+
+    def __init__(self, regularization: float = 0.001):
+        self.regularization = regularization
+
+    def gradient(self, X, y, y_pred, params):
+        """Calculate the gradient of the loss function with respect to the model's parameters"""
+        # Check for bias and adjust if necessary
+        if X.shape[1] != params.shape[0]:
+            X = np.hstack((X, np.ones((X.shape[0], 1))))
+
+        return X.T @ (y_pred - y) / X.shape[0] + self.regularization * params
+
+    def shuffle_data(self, X, y, y_pred):
+        """Shuffle the data for stochastic optimizers"""
+        indices = np.arange(X.shape[0])
+        np.random.shuffle(indices)
+        return X[indices], y[indices], y_pred[indices]
 
     @abstractmethod
     def update(self, X, y, y_pred, params):
--- project1/sgd.py
+++ project1/sgd.py
@@ -5,25 +5,15 @@ from optimizer import Optimizer
 class SGD(Optimizer):
     """Stochastic Gradient Descent optimizer"""
 
-    def __init__(self, learning_rate: float = 0.001, batch_size: int = 1):
+    def __init__(self, learning_rate: float = 0.001, batch_size: int = 16, regularization: float = 0.001):
+        super().__init__(regularization=regularization)
         self.learning_rate = learning_rate
         self.batch_size = batch_size
-        self.regularization = 0.001
-
-    def gradient(self, X, y, y_pred, params):
-        # Check for bias and adjust if necessary
-        if X.shape[1] != params.shape[0]:
-            X = np.hstack((X, np.ones((X.shape[0], 1))))
-
-        return X.T @ (y_pred - y) / X.shape[0] + self.regularization * params
 
     def update(self, X, y, y_pred, params):
-        # Shuffle the data
-        indices = np.arange(X.shape[0])
-        np.random.shuffle(indices)
-        X, y, y_pred = X[indices], y[indices], y_pred[indices]
+        X, y, y_pred = self.shuffle_data(X, y, y_pred)
 
-        # Perform the update
+        # Go through the data in batches
         for i in range(0, X.shape[0], self.batch_size):
             X_batch = X[i : i + self.batch_size]
             y_batch = y[i : i + self.batch_size]
