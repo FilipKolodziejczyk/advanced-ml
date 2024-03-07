8a8ee69dd52a7d540316060df4b41fb4c50e7b57
Filip Kołodziejczyk <filip.kolodziejczyk.00@gmail.com>
2024-03-07 18:37:24 +0100

    Now properly using batches in SGD

--- project1/sgd.py
+++ project1/sgd.py
@@ -5,10 +5,30 @@ from optimizer import Optimizer
 class SGD(Optimizer):
     """Stochastic Gradient Descent optimizer"""
 
-    def __init__(self, learning_rate: float = 1):
+    def __init__(self, learning_rate: float = 0.001, batch_size: int = 1):
         self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.regularization = 0.001
 
-    def update(self, X, y, weights, predictions):
-        gradient = (X.T @ (predictions - y)) / y.size
-        new_weights = weights - self.learning_rate * gradient
-        return new_weights
+    def gradient(self, X, y, y_pred, params):
+        # Check for bias and adjust if necessary
+        if X.shape[1] != params.shape[0]:
+            X = np.hstack((X, np.ones((X.shape[0], 1))))
+
+        return X.T @ (y_pred - y) / X.shape[0] + self.regularization * params
+
+    def update(self, X, y, y_pred, params):
+        # Shuffle the data
+        indices = np.arange(X.shape[0])
+        np.random.shuffle(indices)
+        X, y, y_pred = X[indices], y[indices], y_pred[indices]
+
+        # Perform the update
+        for i in range(0, X.shape[0], self.batch_size):
+            X_batch = X[i : i + self.batch_size]
+            y_batch = y[i : i + self.batch_size]
+            y_pred_batch = y_pred[i : i + self.batch_size]
+            grad = self.gradient(X_batch, y_batch, y_pred_batch, params)
+            params -= self.learning_rate * grad
+
+        return params
